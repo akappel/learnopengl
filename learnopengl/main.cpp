f0af7cecf207c1b6089510cd144618117cadd6a0
akappel <atkappel@gmail.com>
2017-03-22 18:33:24 -0400

    Revert "Created struct for triangles Not necessary really, but I just wanted to try out creating C-structs."
    This reverts commit bae36de2fa1fbb4596c32fee23811438856df03f.

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -23,15 +23,23 @@ void CheckForProgramErrors(GLuint programId);
 // function callbacks
 void CloseWindowCB(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-// simple struct for vao + vertices
-typedef struct triangle {
-	GLuint vaoId;
-	GLfloat vertices[9];
-} triangle_t;
-
 GLFWwindow* window;
 
+GLfloat trigAVertices[] = {
+	 0.0f,  0.8f, 0.0f, 
+	-0.5f,  0.1f, 0.0f, 
+	 0.5f,  0.1f, 0.0f
+};
+
+GLfloat trigBVertices[] = {
+	 0.0f, -0.8f, 0.0f,
+	-0.5f, -0.1f, 0.0f,
+	 0.5f, -0.1f, 0.0f
+};
+
 // ID storage for different OpenGL objects
+GLuint trigAId;
+GLuint trigBId;
 GLuint vertexShaderId;
 GLuint fragmentShaderId;
 GLuint shaderProgramId;
@@ -48,30 +56,12 @@ int main() {
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
-	// give a callback to close the window on ESC key press
+	// Give a callback to close the window on ESC key press
 	glfwSetKeyCallback(window, CloseWindowCB);
 
-	// create triangle objects
-	triangle_t trigA = {
-		0,
-		{	 
-			 0.0f,  0.8f, 0.0f,
-			-0.5f,  0.1f, 0.0f,
-			 0.5f,  0.1f, 0.0f
-		}
-	};
-	triangle_t trigB = {
-		0,
-		{	 
-			 0.0f, -0.8f, 0.0f,
-			-0.5f, -0.1f, 0.0f,
-			 0.5f, -0.1f, 0.0f
-		}
-	};
-
-	// generate a triangle VAOs
-	CreateTriangle(&trigA.vaoId, trigA.vertices, sizeof(trigA.vertices));
-	CreateTriangle(&trigB.vaoId, trigB.vertices, sizeof(trigB.vertices));
+	// generate a triangle
+	CreateTriangle(&trigAId, trigAVertices, sizeof(trigAVertices));
+	CreateTriangle(&trigBId, trigBVertices, sizeof(trigBVertices));
 
 	// setup shaders
 	CreateShaders();
@@ -96,8 +86,8 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		DrawTriangle(&trigA.vaoId);
-		DrawTriangle(&trigB.vaoId);
+		DrawTriangle(&trigAId);
+		DrawTriangle(&trigBId);
 
 		// display results of rendering
 		glfwSwapBuffers(window);
