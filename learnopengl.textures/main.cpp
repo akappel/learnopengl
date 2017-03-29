034dd032a20d5a2e8a0808984a83f822593ae3c6
akappel <atkappel@gmail.com>
2017-03-29 01:15:29 -0400

    Added rect creation, drawing functions

--- learnopengl.textures/main.cpp
+++ learnopengl.textures/main.cpp
@@ -1,6 +1,7 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <SOIL.h>
 #include <iostream>
 
 #include "shader.h"
@@ -10,7 +11,10 @@ int InitGLEW();
 
 // object creation
 void CreateTriangle(GLuint* id, GLfloat* vertices, GLuint size);
+void CreateRect(GLuint* id, GLfloat* vertices, GLuint* indices, GLuint sizeVertices, GLuint sizeIndices);
+void CreateTexture(GLuint *texture);
 void DrawTriangle(GLuint* id);
+void DrawRect(GLuint* id);
 
 // function callbacks
 void CloseWindowCB(GLFWwindow* window, int key, int scancode, int action, int mode);
@@ -24,7 +28,6 @@ int main()
 		return -1;
 	}
 
-	int width, height;
 	GLfloat trigAVertices[] = {
 		// positions		// colors
 		 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
@@ -32,9 +35,28 @@ int main()
 		 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
 	};
 	GLuint trigAId;
+
+	GLfloat rectAVertices[] = {
+		 // Positions          // Colors           // Texture Coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top Right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom Right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom Left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top Left 
+	};
+	GLuint indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+	GLuint rectAId;
+
+	GLuint textureId;
 	Shader shader("./shader.vs", "./shader.frag");
 
+	// setup texture
+	CreateTexture(&textureId);
+
 	// setup viewport width and height based on retrieved values from GLFW
+	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
@@ -43,6 +65,9 @@ int main()
 
 	// generate triangle VAOs
 	CreateTriangle(&trigAId, trigAVertices, sizeof(trigAVertices));
+
+	// generate rect VAO
+	CreateRect(&rectAId, rectAVertices, indices, sizeof(rectAVertices), sizeof(indices));
 
 	while (!glfwWindowShouldClose(window)) {
 		// check for events, such as a keypress
@@ -63,6 +88,20 @@ int main()
 
 	glfwTerminate();
 	return 0;
+}
+
+void CreateTexture(GLuint *texture)
+{
+	glGenTextures(1, texture);
+
+	int texWidth, texHeight;
+	unsigned char* image = SOIL_load_image("./container.jpg", &texWidth, &texHeight, 0, SOIL_LOAD_RGB);
+	glBindTexture(GL_TEXTURE_2D, *texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void CreateTriangle(GLuint* id, GLfloat* vertices, GLuint size)
@@ -92,10 +131,55 @@ void CreateTriangle(GLuint* id, GLfloat* vertices, GLuint size)
 
 }
 
+void CreateRect(GLuint* id, GLfloat* vertices, GLuint* indices, GLuint sizeVertices, GLuint sizeIndices)
+{
+	glGenVertexArrays(1, id);
+
+	// we first need to bind the VAO
+	glBindVertexArray(*id);
+
+	// now generate, bind, and attribute our VBO
+	GLuint vboId;
+	glGenBuffers(1, &vboId);
+	glBindBuffer(GL_ARRAY_BUFFER, vboId);
+	glBufferData(GL_ARRAY_BUFFER, sizeVertices, vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// texture attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
+
+	// finally, setup our indice EBO
+	GLuint eboId;
+	glGenBuffers(1, &eboId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboId);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeIndices, indices, GL_STATIC_DRAW);
+
+	// cleanup
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+}
+
 void DrawTriangle(GLuint* id)
 {
 	glBindVertexArray(*id);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindVertexArray(0);
+}
+
+void DrawRect(GLuint* id)
+{
+	glBindVertexArray(*id);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 }
 
